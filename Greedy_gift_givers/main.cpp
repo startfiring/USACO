a9cc54216166f20ddf6dea7b4ad58213fb46c29c
startfiring <kang_zian@hotmail.com>
2020-07-29 21:58:29 +0800

    7/29

---
+++ Greedy_gift_givers/main.cpp
@@ -0,0 +1,61 @@
+/*
+ID:kang_zi1
+PROG:gift1
+LANG:C++
+*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+struct Person
+{
+    string name;
+    int money;
+} p[10];
+
+int main()
+{
+        ofstream fout("gift1.out");
+        ifstream fin("gift1.in");
+	int a,m,n,money,num;
+	string name;
+	fin>>a;
+	for(int i=0;i<a;i++)
+	{
+		fin>>p[i].name;
+	}
+	for(int j=0;j<a;j++)
+	{
+		fin>>name;
+		fin>>money>>num;
+		for(m=0;m<a;m++)
+		    if(p[m].name==name)
+				break;
+			if(num!=0)
+				p[m].money+=money%num;
+		    p[m].money-=money;
+		
+		    for(m=0;m<num;m++)
+			{
+				fin>>name;
+			    for(n=0;n<a;n++)
+				{
+					if(p[n].name==name)
+					{
+						p[n].money+=money/num;
+                                                break;
+					}
+
+
+				}
+			}
+		
+	}
+	for(n=0;n<a;n++)
+	{
+		fout<<p[n].name<<" "<<p[n].money<<endl;
+	}
+	return 0;
+
+}
