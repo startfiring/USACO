534db0bb9d0214d2ef98c29f2d27b70db67aeba2
startfiring <kang_zian@hotmail.com>
2020-08-02 14:44:35 +0800

    8/2

---
+++ Friday_the_thirteenth/main.cpp
@@ -0,0 +1,44 @@
+/*
+ID:jsntrdy1
+PROG: friday
+LANG: C++
+*/
+
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<fstream>
+
+using namespace std;
+
+ifstream fin(".\\Friday_the_thirteenth\\friday.in");
+ofstream fout(".\\Friday_the_thirteenth\\friday.out"); 
+
+int days[2][12]={31,28,31,30,31,30,31,31,30,31,30,31,
+                 31,29,31,30,31,30,31,31,30,31,30,31};
+
+int week[7];
+int main()
+{
+    int n;
+    fin>>n;
+    int j;
+    int day=0;
+    for(int i=1900;i<=1900+n-1;i++)
+    {
+        int isLeap=((i%4==0&&i%100!=0)||i%400==0)?1:0;
+        for(int j=0;j<=11;j++)
+        {
+            week[(day+13)%7]++;
+            day+=days[isLeap][j];   
+        }   
+
+    }
+    fout<<week[6]<<' ';
+    for(int i=0;i<5;i++)
+      fout<<week[i]<<' ';
+    fout<<week[5]<<endl;
+    fin.close();
+    fout.close();
+    return 0;
+}
