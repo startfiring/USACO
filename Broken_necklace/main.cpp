e567c9fcff349e93a6c97083d2d72d0a3143ca3f
startfiring <kang_zian@hotmail.com>
2020-08-04 10:44:44 +0800

    8/4

---
+++ Broken_necklace/main.cpp
@@ -0,0 +1,49 @@
+/*
+ID: yinzong2
+PROG: beads
+LANG: C++
+*/
+#define MARK
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+int n;
+string s;
+int main() {
+#ifdef MARK
+    freopen(".\\Broken_necklace\\beads.in", "r" ,stdin);
+    freopen(".\\Broken_necklace\\beads.out", "w", stdout);
+#endif
+    cin >> n >> s;
+    s = s+s;
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        char c = s[i];
+        int cnt = 0;
+        int state = 0;
+        if ('w' == c) {
+            state = 0;
+        } else {
+            state = 1;
+        }
+        int j = i;
+        while (state <= 2) {
+            while (j < n+i && ((s[j] == c) || 'w' == s[j])) {
+                j++;
+                cnt++;
+            }
+            state++;
+            c = s[j];
+        }
+        if (ans < cnt){
+            ans = cnt;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
